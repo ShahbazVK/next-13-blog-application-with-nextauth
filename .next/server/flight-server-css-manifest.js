83fc132999fbf6bbdaad07d22c388f5e1898392b
Shahbaz Ali <shahbazalivk@gmail.com>
2023-09-16 04:34:57 +0500

    completed

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\app\\layout.tsx": [
+      "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\app\\page": [
+      "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\app\\globals.css",
+      "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\app\\create\\page": [
+      "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\app\\globals.css",
+      "C:\\Users\\shahb\\Downloads\\Compressed\\Nextjs13-blog-main\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
